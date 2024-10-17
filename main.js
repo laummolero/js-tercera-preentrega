2770d5007c5aeadf94cb7c05f734ac8e27be7262
laury mosquera <laury.25.mosquera@gmail.com>
2024-10-17 17:01:18 -0300

    modificacion con querySelector

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //funcion para cargar el inventario desde localStorage
 function loadInventory() {
   let inventory = JSON.parse(localStorage.getItem("inventory")) || [];
-  let inventoryList = document.getElementById("inventoryList");
+  let inventoryList = document.querySelector("#inventoryList");
   inventoryList.innerHTML = ""; //evita que se los items agregados anteriormente se vuelvan a repetir al ingresar un nuevo item
 
   inventory.forEach((item) => {
@@ -13,13 +13,13 @@ function loadInventory() {
 
 //funcion para agregar un item al inventario
 function addItem() {
-  let itemNumber = document.getElementById("itemNumber").value;
-  let itemName = document.getElementById("itemName").value;
-  let messageDiv = document.getElementById("message");
+  let itemNumber = document.querySelector("#itemNumber").value;
+  let itemName = document.querySelector("#itemName").value;
+  let messageDiv = document.querySelector("#message");
 
   //validacion de entrada
   if (!itemNumber || !itemName) {
-    messageDive.textContent = "Please, complete both boxes";
+    messageDiv.textContent = "Please, complete both boxes";
     return;
   }
 
@@ -27,16 +27,19 @@ function addItem() {
   inventory.push({ number: itemNumber, name: itemName });
   localStorage.setItem("inventory", JSON.stringify(inventory));
 
+  //mensaje en consola
+  console.log(`Item added: Number: ${itemNumber}, Name: ${itemName}`);
+
   messageDiv.textContent = "Item added successfully.";
   loadInventory();
 
   //limpiar las cajas
-  document.getElementById("itemNumber").value = "";
-  document.getElementById("itemName").value = "";
+  document.querySelector("#itemNumber").value = "";
+  document.querySelector("#itemName").value = "";
 }
 
 //evento para el boton
-document.getElementById("addItemButton").addEventListener("click", addItem);
+document.querySelector("#addItemButton").addEventListener("click", addItem);
 
 //cargar el inventario al iniciar la pagina
 loadInventory();
